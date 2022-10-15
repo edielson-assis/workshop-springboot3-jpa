1fa4969968f9b76bb8204e3d2038a50b076ee6e1
Edielson Assis <edielsonnice@hotmail.com>
2022-10-15 20:00:52 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -12,23 +12,29 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String description;
+    private Double price;
+    private String imgURL;
 
     @Transient
-    private Set<Product> products = new HashSet<>();
-
-    public Category(){
+    private Set<Category> categories = new HashSet<>();
+    
+    public Product() {
     }
 
-    public Category(Long id, String name) {
+    public Product(Long id, String name, String description, Double price, String imgURL) {
         this.id = id;
         this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgURL = imgURL;
     }
 
     public Long getId() {
@@ -46,9 +52,33 @@ public class Category implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+
+    public void setImgURL(String imgURL) {
+        this.imgURL = imgURL;
+    }
     
-    public Set<Product> getProducts() {
-        return products;
+    public Set<Category> getCategories() {
+        return categories;
     }
 
     @Override
@@ -67,12 +97,12 @@ public class Category implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Category other = (Category) obj;
+        Product other = (Product) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
         } else if (!id.equals(other.id))
             return false;
         return true;
-    }
+    }    
 }
